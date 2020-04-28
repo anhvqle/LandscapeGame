8060ed815e5990b15e48747a52446d6b2be6dced
Anh Le <43076815+anhvqle@users.noreply.github.com>
2020-04-28 16:17:30 -0400

    Add files via upload

---
+++ Teleporting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+        Debug.Log(transform.position);
+    }
+}
